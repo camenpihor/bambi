9c2c23a321de49e8a2114dfc01c7ca0d75989fae
Camen <camenpihor@gmail.com>
2019-09-04 20:54:25 -0700

    Add documentation to base.py

--- bambi/backends/base.py
+++ bambi/backends/base.py
@@ -1,21 +1,23 @@
-from abc import ABCMeta, abstractmethod
+"""Base backend from which classes are created for available backends."""
+# pylint: disable=unnecessary-pass
+# https://docs.python.org/3/library/abc.html uses pass
+from abc import ABC, abstractmethod
 
 
-class BackEnd:
+class BackEnd(ABC):
+    """Base class for available backends."""
 
-    '''
-    Base class for BackEnd hierarchy.
-    '''
-    __metaclass__ = ABCMeta
+    @abstractmethod
+    def _convert_to_results(self):
+        """Convert the backend results to Bambi-readable results."""
+        pass
 
     @abstractmethod
     def build(self):
+        """Build the model using the backend."""
         pass
 
     @abstractmethod
     def run(self):
-        pass
-
-    @abstractmethod
-    def _convert_to_results(self):
+        """Run the model using the backend."""
         pass
